07d4171a977a95c004007f72c7a2cbd7fcf50132
Yoncho <yoncho.yonchrev@p3-group.com>
2023-08-02 15:50:02 +0100

    tasks 1-5

---
+++ app/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+
+namespace task1
+{
+    public class OddOrEven
+    {
+        public static void Result()
+        {
+            Console.WriteLine("Your number = ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even" + " " + number);
+            }
+            else
+            {
+                Console.WriteLine("odd" + " " + number);
+            }
+        }
+    }
+}
+
+namespace task2
+{
+    public class MoonGravity
+    {
+        public static void Result()
+        {
+            double weight = Convert.ToDouble(Console.ReadLine()) * 0.17;
+            Console.WriteLine(String.Format("{0:0.000}", weight));
+
+        }
+    }
+}
+
+namespace task3
+{
+    public class DivideBy7And5
+    {
+        public static void Result()
+        {
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 5 == 0 && number % 7 == 0)
+            {
+                Console.WriteLine("true" + " " + number);
+            }
+            else
+            {
+                Console.WriteLine("false" + " " + number);
+            }
+
+        }
+    }
+}
+
+namespace task4
+{
+    public class Rectangles
+    {
+        public static void Result()
+        {
+            double widht = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double area = widht * height;
+            double perimeter = widht * 2 + height * 2;
+
+            Console.WriteLine(String.Format("{0:0.00} {1:0.00}", area, perimeter));
+
+        }
+    }
+}
+
+namespace task5
+{
+    public class ThirdDigit
+    {
+        public static void Result()
+        {
+            int widht = Convert.ToInt32(Console.ReadLine());
+            widht %= 1000; 
+            if (widht/100 == 7 )
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false " + widht/100);
+            }
+        }
+    }
+}
+
+namespace task6
+{
+    public class FourDigits
+    {
+        public static void Result()
+        {
+            
+        }
+    }
+}
+
+namespace MainNS
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // task1.OddOrEven.Result();
+            // task2.MoonGravity.Result();
+            // task3.DivideBy7And5.Result();
+            // task4.Rectangles.Result();
+            // task5.ThirdDigit.Result();
+            task6.FourDigits.Result();  
+
+        }
+    }
+}
